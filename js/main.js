3ca795a33d0bcf678ebf588eeb5896731815c33b
Lilian2408 <okaforlilian92@gmail.com>
2023-09-02 15:10:31 -0500

    completed the project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const menuOpen = document.querySelector('.ri-menu-line');
+const menuClose = document.querySelector('.ri-close-circle-fill');
+const nav = document.querySelector('nav ul')
+
+menuOpen.addEventListener('click', function(){
+    nav.style.top ='35%';
+    menuOpen.style.display = 'none';
+    menuClose.style.display = 'block';
+})
+
+menuClose.addEventListener('click', function(){
+    nav.style.top ='-35%';
+    menuClose.style.display = 'none';
+    menuOpen.style.display = 'block';
+})
+
